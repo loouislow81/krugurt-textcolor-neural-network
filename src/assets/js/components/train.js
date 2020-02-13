571eb8a2b4887a66f374991b8c7c41b1d6bab77d
Loouis Low <loouis@gmail.com>
2020-02-14 07:45:54 +0800

    refactor codebase

--- src/assets/js/components/train.js
+++ src/assets/js/components/train.js
@@ -4,10 +4,10 @@ const image = document.getElementById("imageInput");
 const rgb = extractColor(image);
 
 // set rgb color in an array
-const inputs = inputColor();
+const inputs = dataset();
 
-// set neurons
-const inputLayer = 3; // (RRR, GGG, BBB)
+// set neurons for each layers
+const inputLayer = 3; // (R,G,B)
 const hiddenLayer = 3; // value set in between inputLayer and outputLayer
 const outputLayer = 2; // 2 results, either is black or white
 
@@ -20,10 +20,10 @@ const nn = new NeuralNetwork(inputLayer, hiddenLayer, outputLayer);
 // learning_rate by default is 0.1
 // activation function is sigmoid
 const iteration = 100;
-const pixelSize = 300;
+const threshold = 300;
 
 
-function inputColor() {
+function dataset() {
   let r = rgb.r;
   let g = rgb.g;
   let b = rgb.b;
@@ -40,31 +40,37 @@ function nnlayers() {
 
 
 function train() {
-  inputColor();
-  for (let i = 1; i < iteration; i++) {
-    const inputs = inputColor();
+  for (let i = 0; i < iteration; i++) {
+    const inputs = dataset();
     let supervised_target;
-    if (inputs[0] + inputs[1] + inputs[2] > pixelSize) {
-      supervised_target = [1, 0]; // iswhite
+    // supervised_target is (r+g+b) = threshold(t),
+    // for 2 outputs is [tA,tB],
+    // if t value is bigger than either A or B,
+    // then set [1,0] is a bigger-than is black,
+    // meanwhile set [0,1] is a smaller-than is white.
+    if (inputs[0] + inputs[1] + inputs[2] > threshold) {
+      supervised_target = [1, 0];
     }
     else {
-      supervised_target = [0, 1]; // isBlack
+      supervised_target = [0, 1];
     }
-    nn.train( // input_array, target_array
-      [
+    // normalized input values to look like this,
+    // [ 0.8092408156056761, 0.1491446859764432 ]
+    nn.train([
         inputs[0] / 255, // R
         inputs[1] / 255, // G
         inputs[2] / 255  // B
       ], supervised_target
     );
-    log("(Neural Network) training... iteration:", i);
+    log("(NN) training... iteration:", i);
   }
-  log("(Neural Network) end training!", "");
 }
 
 
 function predict() {
-  const predict = nn.predict([ // input_array
+  // normalized output values to look like this,
+  // [ 0.8092408156056761, 0.1491446859764432 ]
+  const outputs = nn.predict([
     inputs[0] / 255, // R
     inputs[1] / 255, // G
     inputs[2] / 255  // B
@@ -75,13 +81,22 @@ function predict() {
   const backgroundColor = document.getElementById("bgColor");
   const foregroundResult = document.getElementById("textResult");
   const backgroundResult = document.getElementById("textRgb");
-  if (predict[0] > predict[1]) {
+  const nnResult = document.getElementById("textNnResult");
+  const rgbSum = inputs[0] + inputs[1] + inputs[2];
+  if (rgbSum > threshold) {
+    log("(NN) trained threshold value", rgbSum + " is bigger than " + threshold);
+  } else {
+    log("(NN) trained threshold value", rgbSum + " is smaller than " + threshold);
+  }
+  log("(NN) output result", outputs);
+  if (outputs[0] > outputs[1]) {
     foregroundColor.style.color = isBlack;
     foregroundColor.style.textShadow = "0px 0px 5px #ffffff";
     backgroundColor.style.backgroundColor = "rgb(" + inputs + ")";
     foregroundResult.innerHTML = isBlack;
     backgroundResult.innerHTML = inputs;
-    log("(Neural Network) predicted text color is", isBlack);
+    nnResult.innerHTML = "1>0 ~ [ " + outputs + " ]";
+    log("(NN) predicted text color is", isBlack);
   }
   else {
     foregroundColor.style.color = isWhite;
@@ -89,13 +104,14 @@ function predict() {
     backgroundColor.style.backgroundColor = "rgb(" + inputs + ")";
     foregroundResult.innerHTML = isWhite;
     backgroundResult.innerHTML = inputs;
-    log("(Neural Network) predicted text color is", isWhite);
+    nnResult.innerHTML = "0<1 ~ [ " + outputs + " ]";
+    log("(NN) predicted text color is", isWhite);
   }
 }
 
 
 log("(Image) average color RGB:", inputs);
-log("(Neural Network) input/hidden/output layers:", layers);
+log("(NN) input/hidden/output model:", layers);
 
 train() // see `assets/js/components/train.js`
 predict()
