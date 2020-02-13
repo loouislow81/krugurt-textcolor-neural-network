6541f47171cd6e31ce1a486a08e1fcbfc2deb01c
Loouis Low <loouis@gmail.com>
2020-02-13 16:06:44 +0800

    clean up codebase

--- src/assets/js/components/train.js
+++ src/assets/js/components/train.js
@@ -3,10 +3,16 @@
 const image = document.getElementById("imageInput");
 const rgb = extractColor(image);
 
+// set rgb color in an array
+const inputs = inputColor();
+
 // set neurons
 const inputLayer = 3; // (RRR, GGG, BBB)
 const hiddenLayer = 3; // value set in between inputLayer and outputLayer
 const outputLayer = 2; // 2 results, either is black or white
+
+// set network settings in an array
+const layers = nnlayers();
 
 const nn = new NeuralNetwork(inputLayer, hiddenLayer, outputLayer);
 
@@ -39,57 +45,56 @@ function train() {
     const inputs = inputColor();
     let supervised_target;
     if (inputs[0] + inputs[1] + inputs[2] > pixelSize) {
-      supervised_target = [1, 0];
+      supervised_target = [1, 0]; // iswhite
     }
     else {
-      supervised_target = [0, 1];
+      supervised_target = [0, 1]; // isBlack
     }
-    nn.train(
-      [inputs[0] / 255, inputs[1] / 255, inputs[2] / 255],
-      supervised_target
+    nn.train( // input_array, target_array
+      [
+        inputs[0] / 255, // R
+        inputs[1] / 255, // G
+        inputs[2] / 255  // B
+      ], supervised_target
     );
-    log("(Neural Network) training... iteration:", (i));
+    log("(Neural Network) training... iteration:", i);
   }
   log("(Neural Network) end training!", "");
 }
 
 
 function predict() {
-  const inputs = inputColor();
-  const predict = nn.predict([
-    inputs[0] / 255,
-    inputs[1] / 255,
-    inputs[2] / 255
+  const predict = nn.predict([ // input_array
+    inputs[0] / 255, // R
+    inputs[1] / 255, // G
+    inputs[2] / 255  // B
   ]);
-  const textId = document.getElementById("textColor");
-  const resultId = document.getElementById("textResult");
-  const resultRgbId = document.getElementById("textRgb");
-  const bgId = document.getElementById("bgColor");
+  const isWhite = "white";
+  const isBlack = "black";
+  const foregroundColor = document.getElementById("textColor");
+  const backgroundColor = document.getElementById("bgColor");
+  const foregroundResult = document.getElementById("textResult");
+  const backgroundResult = document.getElementById("textRgb");
   if (predict[0] > predict[1]) {
-    const isBlack = "black";
-    bgId.style.backgroundColor = "rgb(" + inputs + ")";
-    textId.style.color = isBlack;
-    textId.style.textShadow = "0px 0px 5px #ffffff";
-    resultId.innerHTML = isBlack;
-    resultRgbId.innerHTML = inputs;
-    log("(Neural Network) predicted color is", isBlack);
+    foregroundColor.style.color = isBlack;
+    foregroundColor.style.textShadow = "0px 0px 5px #ffffff";
+    backgroundColor.style.backgroundColor = "rgb(" + inputs + ")";
+    foregroundResult.innerHTML = isBlack;
+    backgroundResult.innerHTML = inputs;
+    log("(Neural Network) predicted text color is", isBlack);
   }
   else {
-    const isWhite = "white";
-    bgId.style.backgroundColor = "rgb(" + inputs + ")";
-    textId.style.color = isWhite;
-    textId.style.textShadow = "0px 0px 5px #000000";
-    resultId.innerHTML = isWhite;
-    resultRgbId.innerHTML = inputs;
-    log("(Neural Network) predicted color is", isWhite);
+    foregroundColor.style.color = isWhite;
+    foregroundColor.style.textShadow = "0px 0px 5px #000000";
+    backgroundColor.style.backgroundColor = "rgb(" + inputs + ")";
+    foregroundResult.innerHTML = isWhite;
+    backgroundResult.innerHTML = inputs;
+    log("(Neural Network) predicted text color is", isWhite);
   }
 }
 
 
-const inputs = inputColor();
 log("(Image) average color RGB:", inputs);
-
-const layers = nnlayers();
 log("(Neural Network) input/hidden/output layers:", layers);
 
 train() // see `assets/js/components/train.js`
